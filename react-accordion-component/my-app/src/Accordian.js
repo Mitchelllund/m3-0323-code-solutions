f0075b6fede5b300dfb6651e39c3060a9400a2d3
Mitchell Lund <Mitchlund922@gmail.com>
2023-04-18 01:36:47 +0000

    Updated react-accordion-component.

--- react-accordion-component/my-app/src/Accordian.js
+++ react-accordion-component/my-app/src/Accordian.js
@@ -1,61 +1,45 @@
-import { useState } from "react"
-import './Accordian.css'
+import { useState } from "react";
+import "./Accordion.css"
 
-export default function Accordion() {
+export default function Accordion({info}) {
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const handlePanelClick = (index) => {
+
+  function handleClick(index) {
     setActiveIndex((prevIndex) => (prevIndex === index ? 0 : index));
   };
 
   return (
     <>
-      <Panel
-        title="Hypertext Markup Language"
-        isActive={activeIndex === 1}
-        onShow={() => handlePanelClick(1)}
-      >
-        The HyperText Markup Language or HTML is the standard markup be displayed in a web browser. It is often assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.
-      </Panel>
-      <Panel
-        title="Cascading Style Sheets"
-        isActive={activeIndex === 2}
-        onShow={() => handlePanelClick(2)}
-      >
-        Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.
-      </Panel>
-      <Panel
-        title="Javascript"
-        isActive={activeIndex === 3}
-        onShow={() => handlePanelClick(3)}
-      >
-        JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.
-      </Panel>
+      {info.map((panel, index) => (
+        <Panel
+          key={index}
+          title={panel.title}
+          content={panel.content}
+          isActive={activeIndex === index + 1}
+          onShow={() => handleClick(index + 1)}
+        />
+      ))}
     </>
   );
 }
 
-function Panel({
-  title,
-  children,
-  isActive,
-  onShow
-}) {
-  function handleClick() {
+function Panel({ title, content, isActive, onShow }) {
+  function handlePanelClick() {
     if (isActive) {
       onShow(0);
     } else {
       onShow();
     }
-  };
+  }
 
   return (
     <section className="panel">
-      <h3 onClick={handleClick}>{title}</h3>
+      <h3 onClick={handlePanelClick}>{title}</h3>
       {isActive ? (
-        <p onClick={() => onShow(0)}>{children}</p>
+        <p onClick={() => onShow(0)}>{content}</p>
       ) : (
-        <p style={{ display: 'none' }}>{children}</p>
+        <p style={{ display: "none" }}>{content}</p>
       )}
     </section>
   );
--- react-accordion-component/my-app/src/App.js
+++ react-accordion-component/my-app/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 import Accordian from './Accordian';
 
+const info = [
+  {
+    title: "Hypertext Markup Language",
+    content:
+      "The HyperText Markup Language or HTML is the standard markup be displayed in a web browser. It is often assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.",
+  },
+  {
+    title: "Cascading Style Sheets",
+    content:
+      "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.",
+  },
+  {
+    title: "Javascript",
+    content:
+      "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
+  },
+];
+
 function App() {
   return (
-      <Accordian />
+      <Accordian info={info}/>
   );
 }
 
